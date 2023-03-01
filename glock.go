ddb9ab647bd531e52fe112788fdd0a71e2f48e9f
szmcdull <machine.gw@gmail.com>
2023-02-28 21:10:17 -0800

    owner first

--- glock.go
+++ glock.go
@@ -1,7 +1,6 @@
 package glock
 
 import (
-	"fmt"
 	"sync"
 	"sync/atomic"
 
@@ -17,32 +16,73 @@ type GLock struct {
 	reentranceCount int64 // count of reentrances in the owner goroutine
 }
 
+// o l+ L r+,
 // Lock and returns true if waited
 func (me *GLock) Lock() bool {
 	gid := goid.Get()
-	lockCount := atomic.AddInt64(&me.lockCount, 1)
-	if lockCount == 1 { // first acquire, current goroutine becomes the owner
+
+	// if me.owner == gid {
+	// 	me.reentranceCount++
+	// 	return false
+	// }
+
+	// me.Mutex.Lock()
+	// result := me.owner == 0
+	// me.owner = gid
+	// return result
+
+	if atomic.CompareAndSwapInt64(&me.owner, 0, gid) { // first acquire, current goroutine becomes the owner
+		atomic.AddInt64(&me.lockCount, 1)
+		// if lockCount != 1 {
+		// 	panic(fmt.Errorf(`wrong lockCount %d, 1 expected`, lockCount))
+		// }
+		//if lockCount == 1 {
 		me.Mutex.Lock()
-		me.owner = gid
+		//}
 		me.reentranceCount++
 		return false
-	} else if lockCount > 0 {
-		if me.owner != gid { // locked in another goroutine, wait to acquire
-			me.Mutex.Lock() // wait
-			me.owner = gid  // acquired
+	} else { // locked in another goroutine, wait to acquire
+		atomic.AddInt64(&me.lockCount, 1)
+		// if lockCount <= 1 {
+		// 	panic(fmt.Errorf(`wrong lockCount %d, expecting > 1`, lockCount))
+		// }
+		if me.owner == gid {
 			me.reentranceCount++
-			return true
+			return false
 		}
 
-		// locked in the same goroutine
+		me.Mutex.Lock() // wait
+		me.owner = gid  // acquired
 		me.reentranceCount++
+		return true
+	}
+}
+
+// o l+ L r+,
+// TryLock only locks successfully when a waiting is not needed. This method is always non-blocking
+func (me *GLock) TryLock() bool {
+	gid := goid.Get()
+	if me.owner == gid { // already owned
+		me.Lock()
+		return true
+	}
+	if me.owner != 0 { // owned by another goroutine, failing the try
 		return false
 	}
 
-	panic(fmt.Errorf(`invalid lock count %d`, lockCount))
+	// me.owner == 0, not owned by any goroutine yet. so try to acquire the lock now
+	if atomic.CompareAndSwapInt64(&me.owner, 0, gid) { // first acquire, current goroutine becomes the owner
+		atomic.AddInt64(&me.lockCount, 1)
+		me.owner = gid
+		me.Mutex.Lock()
+		me.reentranceCount++
+		return true
+	} else { // lock grabbed by another goroutine first
+		return false
+	}
 }
 
-// l+ L o r+, r- o U l-
+// o l+ L r+, r- U l- o
 
 func (me *GLock) Unlock() {
 	gid := goid.Get()
@@ -55,16 +95,19 @@ func (me *GLock) Unlock() {
 		panic(`unlocking non-owned GLock`)
 	}
 
-	// lockCount may be increased by other goroutines (before waiting for this lock)
-	// so me.owner may not be cleared after a full unlocking. but it doesn't matter, because lockCount will be 0
-	if me.lockCount == 1 {
-		me.owner = 0
-	}
+	// // lockCount may be increased by other goroutines (before waiting for this lock)
+	// // so me.owner may not be cleared after a full unlocking. but it doesn't matter, because lockCount will be 0
+	// if me.lockCount == 1 {
+	// 	me.owner = 0
+	// }
 
 	if me.reentranceCount == 0 {
 		me.Mutex.Unlock()
 	}
 
-	atomic.AddInt64(&me.lockCount, -1)
+	lockCount := atomic.AddInt64(&me.lockCount, -1)
 
+	if lockCount == 0 {
+		me.owner = 0
+	}
 }
--- glock_test.go
+++ glock_test.go
@@ -33,6 +33,41 @@ func TestReentrance(t *testing.T) {
 	timer := time.NewTimer(time.Second)
 	select {
 	case <-timer.C:
+		t.Error(`timed out`)
+	case <-ch:
+	}
+}
+
+func TestReentranceTry(t *testing.T) {
+	ch := make(chan struct{})
+
+	go func() {
+		l := GLock{}
+		if l.TryLock() != true {
+			t.Fail()
+		}
+		if l.TryLock() != true {
+			t.Error(`2`)
+		}
+		l.Unlock()
+		l.Unlock()
+		if l.owner != 0 {
+			t.Error(`owner not cleared`)
+		}
+
+		if l.reentranceCount != 0 {
+			t.Error(`reentranceCount not cleared`)
+		}
+
+		if l.lockCount != 0 {
+			t.Error(`lockCount not cleared`)
+		}
+		ch <- struct{}{}
+	}()
+
+	timer := time.NewTimer(time.Second)
+	select {
+	case <-timer.C:
 		t.Fail() // timed out
 	case <-ch:
 	}
@@ -44,6 +79,46 @@ func TestLock(t *testing.T) {
 	step := 0
 
 	go func() {
+		l.Lock()
+		step = 1
+		l.Unlock()
+	}()
+
+	time.Sleep(time.Millisecond * 100)
+	if step != 0 {
+		t.Error(`not locked`)
+	}
+
+	l.Unlock()
+	time.Sleep(time.Millisecond * 100)
+	if step != 1 {
+		t.Error(`not released`)
+	}
+
+	if l.owner != 0 {
+		t.Error(`owner not cleared`)
+	}
+
+	if l.reentranceCount != 0 {
+		t.Error(`reentranceCount not cleared`)
+	}
+
+	if l.lockCount != 0 {
+		t.Error(`lockCount not cleared`)
+	}
+}
+
+func TestLockTry(t *testing.T) {
+	l := GLock{}
+	if l.TryLock() != true {
+		t.Error(`1`)
+	}
+	step := 0
+
+	go func() {
+		if l.TryLock() != false {
+			t.Error(`2`)
+		}
 		l.Lock()
 		step = 1
 		l.Unlock()
@@ -99,6 +174,48 @@ func TestConcurrent(t *testing.T) {
 		t.Fail()
 	}
 	if counted != count {
+		t.Fail()
+	}
+}
+
+func TestConcurrentTry(t *testing.T) {
+	l := GLock{}
+	count := 1000000
+	wg := sync.WaitGroup{}
+	wg.Add(count)
+	counted := int64(0)
+
+	for i := 0; i < count; i++ {
+		go func() {
+			c := rand.Intn(16) + 1
+			c2 := 0
+			added := false
+			for j := 0; j < c; j++ {
+				if l.TryLock() {
+					c2++
+				}
+			}
+			if c2 == 0 {
+				l.Lock()
+				c2++
+				added = true
+			}
+			counted++
+			for j := 0; j < c2; j++ {
+				l.Unlock()
+			}
+			if added {
+
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	if l.owner != 0 || l.reentranceCount != 0 || l.lockCount != 0 {
+		t.Fail()
+	}
+	if counted != int64(count) {
 		t.Fail()
 	}
 }
